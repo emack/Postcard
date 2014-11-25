264088e287d03e8afbeebdfbddd0e28a2b980dca
edmar-trimorph <edmar.posesano@trimor.ph>
2014-11-25 14:50:42 +0800

    add func of nametextfield and label

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
@@ -34,7 +35,18 @@ class ViewController: UIViewController {
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+        //for name label and name textfield
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        
+        enterNameTextField.resignFirstResponder()
+        
+        
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+
     }
 
 }
